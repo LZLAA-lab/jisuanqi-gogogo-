f75ca31c21af574531a32664fc26d06784c87d22
LZLAA-lab <372139369@qq.com>
2025-09-25 15:17:10 +0800

    1 commit(Input and Output)

---
+++ N.C.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+#include<cstring>
+#include<string>
+#include<algorithm>
+#include<stack>
+using namespace std;
+int main()
+{
+    string s1;
+    cout<<"Please enter your mathematical expression"<<endl;
+    cin>>s1;
+    double result;
+    cout<<"Here is your result"<<endl;
+    cout<<result;
+    return 0;
+}
