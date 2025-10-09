6e05ee53c20f538025bef12c26730ac82699e569
tqw621 <1094114679@qq.com>
2025-10-09 16:08:28 +0800

    Create ct.cpp

---
+++ ct.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+double add(double a, double b)//加法运算
+{
+	double sum = a + b;
+	return sum;
+}
+double sub(double a, double b)//减法运算
+{
+	double diff = a - b;
+	return diff;
+}
+double mul(double a, double b)//乘法运算
+{
+	double pro = a * b;
+	return pro;
+}
+double chu(double a, double b)//除法运算
+{
+	double quo = a / b;
+	return quo;
+}
